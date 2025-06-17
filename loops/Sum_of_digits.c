91bcf057018574e2ec55b77913e3a17f12b83dcd
Sarvesh S <113613900+SarveshSanthanam@users.noreply.github.com>
2025-06-17 20:15:59 +0530

    Create Sum_of_digits.c

---
+++ loops/Sum_of_digits.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int val,sum =0 ;
+    printf("Enter the number: ");
+    scanf("%d",&val);
+    while (num/=10)
+        sum += num%10;
+    printf("sum = %d",sum);
+}
